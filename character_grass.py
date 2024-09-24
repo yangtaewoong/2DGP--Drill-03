c6a23dea7f8bd2f5f1ff4875144a4da153598e89
yangtaewoong <tom0323@naver.com>
2024-09-24 11:49:54 +0900

    뼈대 완성(프린트로 무한루프 구조 확인)

--- character_grass.py
+++ character_grass.py
@@ -6,68 +6,18 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 400
-y = 90
+def run_circle():
+    print('Circle')
+    pass
 
-tri_size = 200;
-circle_size = 200;
+def run_rectangle():
+    print('Rectangle')
+    pass
 
-stop = True
-while (stop):
-    while (x < 400 + tri_size):
-        #Game Rendering
-        clear_canvas()
-        grass.draw(400,30)
-        character.draw(x,y)
-        update_canvas()
-    
-        #Game Logic
-        x += 2
-        delay(0.01)
-    while (x > 400 and y < 490):
-        #Game Rendering
-        clear_canvas()
-        grass.draw(400,30)
-        character.draw(x,y)
-        update_canvas()
-    
-        #Game Logic
-        x -= 1
-        y += 1.2
-        delay(0.01)
-    while (x > 400 - tri_size and y > 90):
-        #Game Rendering
-        clear_canvas()
-        grass.draw(400,30)
-        character.draw(x,y)
-        update_canvas()
-    
-        #Game Logic
-        x -= 1
-        y -= 1.2
-        delay(0.01)
-    while (x < 400):
-        #Game Rendering
-        clear_canvas()
-        grass.draw(400,30)
-        character.draw(x,y)
-        update_canvas()
-    
-        #Game Logic
-        x += 2
-        delay(0.01)
-    delay(1)
-    for i in range(-90,270):
-        #Game Rendering
-        clear_canvas()
-        grass.draw(400,30)
-        character.draw(x,y)
-        update_canvas()
-    
-        #Game Logic
-        x = 400 + (math.cos((i * math.pi / 180)) * circle_size)
-        y = 90 + circle_size + (math.sin((i * math.pi / 180)) * circle_size)
-        delay(0.01)
-    delay(1)
+
+while True:
+    run_rectangle()
+    run_circle()
+
 
 close_canvas()
